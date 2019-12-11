25f89affb8448decf465de3b0fcdd96ae80140f6
HytonightYX <whosiyuan@qq.com>
2019-12-11 09:01:03 +0800

    hsy: 写文章页面

--- client/src/page/find/index.js
+++ client/src/page/find/index.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import './style.less'
+import { Icon } from 'semantic-ui-react'
+import ICON from '../../asset/icon'
+import FixedBar from '../../component/FixedBar'
+
+const FIND_MENU = [
+	{title: '所有', key: 'all', icon: ''},
+	{title: '关注', key: 'follow', icon: ''},
+	{title: '最热', key: 'hot', icon: ''},
+]
 
 class Find extends React.Component {
 	state = {
-
+		currtab: 'all' // all || follow || hot
 	}
 
 	componentDidMount() {
+
+	}
+
+	doChangeTab = (e) => {
+		let key
+		switch (e.target.innerText) {
+			case '所有':
+				key = 'all'
+				break
+			case '关注':
+				key = 'follow'
+				break
+			case '最热':
+				key = 'hot'
+				break
+			default:
+				key = 'all'
+		}
+		this.setState({currtab: key})
 	}
 
 	render() {
+
+		const {currtab} = this.state
 
 		const TEMPC = () => (
 			<div className="note-card">
@@ -26,11 +56,16 @@ class Find extends React.Component {
 					<div className="note-attr">
 
 						<div className="user">
-
+							<img className="user-icon"
+							     src="https://cdn.sspai.com/2018/10/31/e66cabe8eb4d2b3025021d65881b494b.png?imageMogr2/quality/95/thumbnail/!200x200r/gravity/Center/crop/200x200"
+							     alt=""/>
+							<span className="username">老胡胡胡胡</span>
+							<span className="update-time">3小时前</span>
 						</div>
 
 						<div className="like">
-
+							<div><img src={ICON.bookmark} alt=""/><span>36</span></div>
+							<div><img src={ICON.comment} alt=""/><span>2</span></div>
 						</div>
 					</div>
 				</div>
@@ -39,19 +74,29 @@ class Find extends React.Component {
 
 		return (
 			<div className="g-find">
-				<div className="note-list">
 
-					<TEMPC />
-					<TEMPC />
-					<TEMPC />
-					<TEMPC />
-					<TEMPC />
-					<TEMPC />
-					<TEMPC />
-
+				<div className="m-find-tab">
+					<ul>
+						{FIND_MENU.map(item =>
+							<li key={item.key} onClick={this.doChangeTab}
+							    className={`${currtab === item.key && 'active'}`}
+							>{item.title}</li>
+						)}
+					</ul>
 				</div>
 
-				<div className="right-bar">
+				<div className="m-find">
+					<div className="note-list">
+						<TEMPC/>
+						<TEMPC/>
+						<TEMPC/>
+						<TEMPC/>
+						<TEMPC/>
+						<TEMPC/>
+						<TEMPC/>
+					</div>
+
+					<div className="right-bar">
 
 					</div>
 				</div>
---
+++ client/src/page/write/index.js
@@ -0,0 +1,139 @@
+import * as React from 'react'
+import { Input } from 'semantic-ui-react'
+import BraftEditor from 'braft-editor'
+import { Form, Input as AntInput, Button, Icon, Select, Spin } from 'antd'
+import debounce from 'lodash/debounce'
+
+import 'braft-editor/dist/index.css'
+import './style.less'
+
+const {Option} = Select
+
+
+@Form.create()
+class Write extends React.Component {
+	constructor(props) {
+		super(props)
+		this.lastFetchId = 0
+		this.fetchUser = debounce(this.fetchUser, 800)
+	}
+
+	state = {
+		data: [],
+		value: [],
+		fetching: false
+	}
+
+	fetchUser = value => {
+		console.log('fetching user', value)
+		this.lastFetchId += 1
+		const fetchId = this.lastFetchId
+		this.setState({data: [], fetching: true})
+		fetch('https://randomuser.me/api/?results=5')
+			.then(response => response.json())
+			.then(body => {
+				if (fetchId !== this.lastFetchId) {
+					// for fetch callback order
+					return
+				}
+				const data = body.results.map(user => ({
+					text: `${user.name.first} ${user.name.last}`,
+					value: user.login.username,
+				}))
+				this.setState({data, fetching: false})
+			})
+	}
+
+	handleChange = value => {
+		this.setState({
+			value,
+			data: [],
+			fetching: false,
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.form.validateFields((error, values) => {
+			if (!error) {
+				const submitData = {
+					title: values.title,
+					raw: values.content.toRAW(),
+					html: values.content.toHTML(),
+					tag: ['大一', '高数'],
+					author: 25
+				}
+				console.log(JSON.stringify(submitData))
+			}
+		})
+
+	}
+
+	render() {
+		const {getFieldDecorator, getFieldsError, getFieldError, isFieldTouched} = this.props.form
+		const controls = ['bold', 'italic', 'underline', 'text-color', 'separator', 'link', 'separator', 'media']
+		const {fetching, data, value} = this.state
+
+		return (
+			<div className="g-write">
+				<Form onSubmit={this.handleSubmit}>
+					<Form.Item>
+						{getFieldDecorator('title', {
+							rules: [{required: true, message: '请输入标题'}],
+							initialValue: ''
+						})(
+							<AntInput
+								className="title-input"
+								placeholder="请输入标题"
+							/>,
+						)}
+					</Form.Item>
+
+					<Form.Item>
+						{getFieldDecorator('content', {
+							validateTrigger: 'onBlur',
+							rules: [{
+								required: true,
+								validator: (_, value, callback) => {
+									if (value.isEmpty()) {
+										callback('请输入正文内容')
+									} else {
+										callback()
+									}
+								}
+							}],
+						})(
+							<BraftEditor
+								ref={instance => this.editorInstance = instance}
+								className="m-editor"
+								placeholder="请输入正文内容"
+							/>
+						)}
+					</Form.Item>
+					<Form.Item className="m-flex-row">
+						<Select
+							size="large"
+							mode="multiple"
+							labelInValue
+							value={value}
+							placeholder="Select users"
+							notFoundContent={fetching ? <Spin size="small"/> : null}
+							filterOption={false}
+							onSearch={this.fetchUser}
+							onChange={this.handleChange}
+							style={{width: '400px'}}
+						>
+							{data.map(d => (
+								<Option key={d.value}>{d.text}</Option>
+							))}
+						</Select>
+						<Button size="large" htmlType="submit">提交</Button>
+					</Form.Item>
+				</Form>
+			</div>
+		)
+	}
+
+}
+
+export default Write
