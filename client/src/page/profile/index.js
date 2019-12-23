9894718dab57652df3f094895f1eb30bc28c7c8c
HytonightYX <whosiyuan@qq.com>
2019-12-23 17:38:41 +0800

    hsy: 搭建个人页面

--- client/src/page/profile/index.js
+++ client/src/page/profile/index.js
@@ -1,6 +1,12 @@
+import { Divider } from 'antd'
+import { computed } from 'mobx'
+import { inject, observer } from 'mobx-react'
 import React from 'react'
 import './style.less'
+import { Redirect } from 'react-router'
 
+@inject('userStore')
+@observer
 class Profile extends React.Component {
 
 	state = {
@@ -9,15 +15,43 @@ class Profile extends React.Component {
 		fetching: false
 	}
 
+	@computed
+	get currUser() {
+		return this.props.userStore.currUser
+	}
 
 	render() {
+		console.log(this.currUser)
+
+		if (!this.currUser) {
+			message.info('请先登录!')
+			return <Redirect to='login' />
+		}
 
 		return (
 			<div className="g-profile">
 				<div className="m-container">
 
 					<div className="user-content">
-						user
+						<div className="user-panel">
+							<div className="user-icon">
+								<img src={this.currUser.avatar} alt=""/>
+							</div>
+
+							<div className="user-info">
+								<div>
+									<span className="username">
+										{this.currUser.userName}
+									</span>
+
+									<button className="btn-edit">编辑</button>
+								</div>
+
+								<Divider />
+
+								<span>文章 1 · 被赞 0</span>
+							</div>
+						</div>
 					</div>
 
 					<div className="right-side">
